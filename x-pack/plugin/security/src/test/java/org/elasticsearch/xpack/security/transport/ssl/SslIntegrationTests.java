9d93ca62c6ea069fc4aa06d022f24ba1c7de148f
Armin <me@obrown.io>
2018-07-31 11:14:01 +0200

    repeat 32478

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/ssl/SslIntegrationTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/ssl/SslIntegrationTests.java
@@ -5,7 +5,6 @@
  */
 package org.elasticsearch.xpack.security.transport.ssl;
 
-import com.carrotsearch.randomizedtesting.annotations.Repeat;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -54,7 +53,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 
-@Repeat(iterations = 10)
 public class SslIntegrationTests extends SecurityIntegTestCase {
 
     @Override
