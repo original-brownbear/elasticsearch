48eb511d47bfbc6901b684abf74ab169a065505b
Albert Zaharovits <albert.zaharovits@elastic.co>
2020-03-17 12:48:18 +0200

    encrypted azure and s3 blob store repository

--- plugins/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3BlobStoreRepositoryTests.java
+++ plugins/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3BlobStoreRepositoryTests.java
@@ -145,7 +145,7 @@ public class S3BlobStoreRepositoryTests extends ESMockAPIBasedRepositoryIntegTes
     }
 
     public void testEnforcedCooldownPeriod() throws IOException {
-        final String repoName = createRepository(randomName(), Settings.builder().put(repositorySettings())
+        final String repoName = createRepository(randomRepositoryName(), Settings.builder().put(repositorySettings())
             .put(S3Repository.COOLDOWN_PERIOD.getKey(), TEST_COOLDOWN_PERIOD).build(), randomBoolean());
 
         final SnapshotId fakeOldSnapshot = client().admin().cluster().prepareCreateSnapshot(repoName, "snapshot-old")
