5a89ca34b3daca06e92f273034154416544b8c92
Armin Braun <me@obrown.io>
2021-07-09 18:59:40 +0200

    reporo

--- server/src/internalClusterTest/java/org/elasticsearch/search/basic/SearchWhileRelocatingIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/search/basic/SearchWhileRelocatingIT.java
@@ -8,10 +8,12 @@
 
 package org.elasticsearch.search.basic;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
-import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.search.SearchHits;
@@ -23,26 +25,24 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoTimeout;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.formatShardStatus;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.*;
 import static org.hamcrest.Matchers.equalTo;
 
-@ESIntegTestCase.ClusterScope(minNumDataNodes = 2)
+@ESIntegTestCase.ClusterScope(minNumDataNodes = 2, scope = ESIntegTestCase.Scope.TEST)
 public class SearchWhileRelocatingIT extends ESIntegTestCase {
 
     public void testSearchAndRelocateConcurrentlyRandomReplicas() throws Exception {
-        testSearchAndRelocateConcurrently(randomIntBetween(0, 1));
+        testSearchAndRelocateConcurrently(0);
     }
 
     private void testSearchAndRelocateConcurrently(final int numberOfReplicas) throws Exception {
-        final int numShards = between(1, 20);
+        final int numShards = 20;
         client().admin().indices().prepareCreate("test")
                 .setSettings(Settings.builder().put("index.number_of_shards", numShards).put("index.number_of_replicas", numberOfReplicas))
                 .setMapping("loc", "type=geo_point", "test", "type=text").get();
         ensureGreen();
         List<IndexRequestBuilder> indexBuilders = new ArrayList<>();
-        final int numDocs = between(10, 20);
+        final int numDocs = 20;
         for (int i = 0; i < numDocs; i++) {
             indexBuilders.add(client().prepareIndex("test").setId(Integer.toString(i))
                     .setSource(
@@ -55,51 +55,21 @@ public class SearchWhileRelocatingIT extends ESIntegTestCase {
         for (int i = 0; i < numIters; i++) {
             final AtomicBoolean stop = new AtomicBoolean(false);
             final List<String> nonCriticalExceptions = new CopyOnWriteArrayList<>();
-
-            Thread[] threads = new Thread[scaledRandomIntBetween(1, 3)];
-            for (int j = 0; j < threads.length; j++) {
-                threads[j] = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            while (stop.get() == false) {
-                                SearchResponse sr = client().prepareSearch().setSize(numDocs).get();
-                                if (sr.getHits().getTotalHits().value != numDocs) {
-                                    // if we did not search all shards but had no failures that is potentially fine
-                                    // if only the hit-count is wrong. this can happen if the cluster-state is behind when the
-                                    // request comes in. It's a small window but a known limitation.
-                                    if (sr.getTotalShards() != sr.getSuccessfulShards() && sr.getFailedShards() == 0) {
-                                        nonCriticalExceptions.add("Count is " + sr.getHits().getTotalHits().value + " but " + numDocs +
-                                            " was expected. " + formatShardStatus(sr));
-                                    } else {
-                                        assertHitCount(sr, numDocs);
-                                    }
-                                }
-
-                                final SearchHits sh = sr.getHits();
-                                assertThat("Expected hits to be the same size the actual hits array", sh.getTotalHits().value,
-                                        equalTo((long) (sh.getHits().length)));
-                                // this is the more critical but that we hit the actual hit array has a different size than the
-                                // actual number of hits.
-                            }
-                        } catch (SearchPhaseExecutionException ex) {
-                            // it's possible that all shards fail if we have a small number of shards.
-                            // with replicas this should not happen
-                            if (numberOfReplicas == 1 || ex.getMessage().contains("all shards failed") == false) {
-                                throw ex;
-                            }
-                        }
-                    }
-                };
+            final int parallelSearchers = scaledRandomIntBetween(5, 10);
+            final List<PlainActionFuture<Void>> futures = new ArrayList<>(parallelSearchers);
+            allowNodes("test", 3);
+            for (int k = 0; k < parallelSearchers; k++) {
+                final PlainActionFuture<Void> f = PlainActionFuture.newFuture();
+                runSearches(client(), numDocs, nonCriticalExceptions, numberOfReplicas, stop, f);
+                futures.add(f);
             }
-            for (int j = 0; j < threads.length; j++) {
-                threads[j].start();
+            while (stop.get() == false) {
+                allowNodes("test", between(1, 3));
+                assertAcked(client().admin().cluster().prepareReroute().get());
             }
-            allowNodes("test", between(1, 3));
-            client().admin().cluster().prepareReroute().get();
             stop.set(true);
-            for (int j = 0; j < threads.length; j++) {
-                threads[j].join();
+            for (PlainActionFuture<Void> future : futures) {
+                future.get();
             }
             // this might time out on some machines if they are really busy and you hit lots of throttling
             ClusterHealthResponse resp = client().admin().cluster().prepareHealth().setWaitForYellowStatus()
@@ -113,5 +83,47 @@ public class SearchWhileRelocatingIT extends ESIntegTestCase {
                 }
             }
         }
+    }
+
+    private void runSearches(Client client, int numDocs, List<String> nonCriticalExceptions, int numberOfReplicas, AtomicBoolean stop,
+                             ActionListener<Void> listener) {
+        client.prepareSearch().setSize(numDocs).execute(new ActionListener<>() {
+            @Override
+            public void onResponse(SearchResponse sr) {
+                if (stop.get()) {
+                    listener.onResponse(null);
+                    return;
+                }
+                if (sr.getHits().getTotalHits().value != numDocs) {
+                    // if we did not search all shards but had no failures that is potentially fine
+                    // if only the hit-count is wrong. this can happen if the cluster-state is behind when the
+                    // request comes in. It's a small window but a known limitation.
+                    if (sr.getTotalShards() != sr.getSuccessfulShards() && sr.getFailedShards() == 0) {
+                        nonCriticalExceptions.add("Count is " + sr.getHits().getTotalHits().value + " but " + numDocs +
+                                " was expected. " + formatShardStatus(sr));
+                    } else {
+                        assertHitCount(sr, numDocs);
+                    }
+                }
+
+                final SearchHits sh = sr.getHits();
+                assertThat("Expected hits to be the same size the actual hits array", sh.getTotalHits().value,
+                        equalTo((long) (sh.getHits().length)));
+                // this is the more critical but that we hit the actual hit array has a different size than the
+                // actual number of hits.
+                runSearches(client(), numDocs, nonCriticalExceptions, numberOfReplicas, stop, listener);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                // it's possible that all shards fail if we have a small number of shards.
+                // with replicas this should not happen
+                if (numberOfReplicas == 1 || e.getMessage().contains("all shards failed") == false) {
+                    listener.onFailure(e);
+                } else {
+                    listener.onResponse(null);
+                }
+            }
+        });
     }
 }
