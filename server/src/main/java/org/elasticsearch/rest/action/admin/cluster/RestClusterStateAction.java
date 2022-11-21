0771222366d4f03686b0c873f0e8c9f64d78ff16
Armin Braun <me@obrown.io>
2022-11-21 10:49:46 +0100

    start

--- server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestClusterStateAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestClusterStateAction.java
@@ -18,19 +18,21 @@ import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.collect.Iterators;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.common.xcontent.ChunkedToXContent;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.action.DispatchingRestToXContentListener;
 import org.elasticsearch.rest.action.RestCancellableNodeClient;
+import org.elasticsearch.rest.action.RestChunkedToXContentListener;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xcontent.ToXContent;
-import org.elasticsearch.xcontent.ToXContentObject;
 import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.function.LongSupplier;
@@ -108,21 +110,9 @@ public class RestClusterStateAction extends BaseRestHandler {
         return channel -> new RestCancellableNodeClient(client, request.getHttpChannel()).execute(
             ClusterStateAction.INSTANCE,
             clusterStateRequest,
-            new DispatchingRestToXContentListener<RestClusterStateResponse>(
-                // Process serialization on MANAGEMENT pool since the serialization of the cluster state to XContent
-                // can be too slow to execute on an IO thread
-                threadPool.executor(ThreadPool.Names.MANAGEMENT),
-                channel,
-                request
-            ) {
-                @Override
-                protected ToXContent.Params getParams() {
-                    return new ToXContent.DelegatingMapParams(
-                        singletonMap(Metadata.CONTEXT_MODE_PARAM, Metadata.CONTEXT_MODE_API),
-                        request
-                    );
-                }
-            }.map(response -> new RestClusterStateResponse(clusterStateRequest, response, threadPool::relativeTimeInMillis))
+            new RestChunkedToXContentListener<RestClusterStateResponse>(channel).map(
+                response -> new RestClusterStateResponse(clusterStateRequest, request, response, threadPool::relativeTimeInMillis)
+            )
         );
     }
 
@@ -141,22 +131,32 @@ public class RestClusterStateAction extends BaseRestHandler {
         static final String CLUSTER_NAME = "cluster_name";
     }
 
-    private static class RestClusterStateResponse implements ToXContentObject {
+    private static class RestClusterStateResponse implements ChunkedToXContent {
 
         private final ClusterStateRequest request;
         private final ClusterStateResponse response;
         private final LongSupplier currentTimeMillisSupplier;
         private final long startTimeMillis;
 
-        RestClusterStateResponse(ClusterStateRequest request, ClusterStateResponse response, LongSupplier currentTimeMillisSupplier) {
+        final ToXContent.Params updatedParams;
+
+        RestClusterStateResponse(
+            ClusterStateRequest request,
+            RestRequest restRequest,
+            ClusterStateResponse response,
+            LongSupplier currentTimeMillisSupplier
+        ) {
             this.request = request;
             this.response = response;
             this.currentTimeMillisSupplier = currentTimeMillisSupplier;
             this.startTimeMillis = currentTimeMillisSupplier.getAsLong();
+            this.updatedParams = new ToXContent.DelegatingMapParams(
+                singletonMap(Metadata.CONTEXT_MODE_PARAM, Metadata.CONTEXT_MODE_API),
+                restRequest
+            );
         }
 
-        @Override
-        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        public XContentBuilder toXContent(XContentBuilder builder, ToXContent.Params params) throws IOException {
             if (request.local() == false
                 && currentTimeMillisSupplier.getAsLong() - startTimeMillis > request.masterNodeTimeout().millis()) {
                 throw new ElasticsearchTimeoutException("Timed out getting cluster state");
@@ -172,6 +172,11 @@ public class RestClusterStateAction extends BaseRestHandler {
             }
             builder.endObject();
             return builder;
+        }
+
+        @Override
+        public Iterator<? extends ToXContent> toXContentChunked() {
+            return Iterators.single((b, p) -> this.toXContent(b, updatedParams));
         }
     }
 
