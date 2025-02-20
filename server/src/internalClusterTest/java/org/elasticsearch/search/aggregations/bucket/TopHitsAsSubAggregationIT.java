3116c0cceff2b6beef67e800940e3226ef30e6f5
Armin Braun <me@obrown.io>
2025-02-20 13:16:32 +0100

    bck

--- server/src/internalClusterTest/java/org/elasticsearch/search/aggregations/bucket/TopHitsAsSubAggregationIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/search/aggregations/bucket/TopHitsAsSubAggregationIT.java
@@ -9,16 +9,23 @@
 
 package org.elasticsearch.search.aggregations.bucket;
 
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchService;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.xcontent.ToXContent;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoFailuresAndResponse;
 import static org.elasticsearch.xcontent.XContentFactory.jsonBuilder;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class TopHitsAsSubAggregationIT extends ESIntegTestCase {
 
@@ -28,6 +35,7 @@ public class TopHitsAsSubAggregationIT extends ESIntegTestCase {
             .put(super.nodeSettings(nodeOrdinal, otherSettings))
             .put("thread_pool.search.size", randomIntBetween(1, 10))
             .put("thread_pool.search.queue_size", 1000 * randomIntBetween(1, 10))
+            .put(SearchService.MINIMUM_DOCS_PER_SLICE.getKey(), randomIntBetween(1, 500))
             .build();
     }
 
@@ -46,6 +54,11 @@ public class TopHitsAsSubAggregationIT extends ESIntegTestCase {
                         .startObject("nested_value")
                         .field("type", "nested")
                         .field("dynamic", true)
+                        .startObject("properties")
+                        .startObject("foo")
+                        .field("type", "keyword")
+                        .endObject()
+                        .endObject()
                         .endObject()
                         .endObject()
                         .endObject()
@@ -56,7 +69,20 @@ public class TopHitsAsSubAggregationIT extends ESIntegTestCase {
                 final String id = i + "_" + j;
                 if (randomBoolean()) {
                     prepareIndex(indexName).setId(id)
-                        .setSource("numeric_value", i % 10, "nested_value", Map.of("foo", "bar-" + i, "round", j))
+                        .setSource(
+                            "numeric_value",
+                            i % 10,
+                            "rare_value",
+                            rarely() && rarely() ? "a" : "b",
+                            "nested_value",
+                            randomBoolean()
+                                ? Map.of("foo", "bar-" + i, "round", j)
+                                : List.of(
+                                    Map.of("foo", "bar-" + i, "round", j),
+                                    Map.of("foo", "bar-" + randomNonNegativeLong(), "round", j),
+                                    Map.of("foo", "bar-" + randomNonNegativeLong())
+                                )
+                        )
                         .get();
                 } else {
                     prepareIndex(indexName).setId(id)
@@ -64,49 +90,59 @@ public class TopHitsAsSubAggregationIT extends ESIntegTestCase {
                             "numeric_value",
                             i,
                             "nested_value",
-                            randomList(
-                                100,
-                                () -> randomBoolean()
-                                    ? randomNestedA(randomInt(100), randomInt(100))
-                                    : randomNestedB(randomInt(100), randomInt(100))
-                            )
+                            randomBoolean()
+                                ? List.of()
+                                : randomList(
+                                    100,
+                                    () -> randomBoolean()
+                                        ? randomNestedA(randomInt(100), randomInt(100))
+                                        : randomNestedB(randomInt(100), randomInt(100))
+                                )
                         )
                         .get();
                 }
             }
             flushAndRefresh(indexName);
+            indicesAdmin().prepareForceMerge(indexName).setMaxNumSegments(5).setFlush(true).get();
+            final AtomicReference<String> agg = new AtomicReference<>();
             for (int k = 0; k < 100; k++) {
                 assertNoFailuresAndResponse(
-                    prepareSearch(indexName).setSize(randomInt(5))
+                    prepareSearch(indexName).setSize(0)
                         .addAggregation(
-                            AggregationBuilders.range("some_ranges")
-                                .field("numeric_value")
-                                .addRange(0, 1)
-                                .addRange(1, 2)
-                                .addUnboundedFrom(2)
+                            AggregationBuilders.nested("nested", "nested_value")
                                 .subAggregation(
-                                    AggregationBuilders.nested("nested", "nested_value")
-                                        .subAggregation(
-                                            AggregationBuilders.topHits("some_top_hits")
-                                                .fetchSource("nested_value.round", null)
-                                                .size(100)
-                                                .sort("nested_value.round")
+                                    AggregationBuilders.topHits("some_top_hits")
+                                        .fetchSource(new String[] { "nested_value.round", "nested_value.foo", "rare_value" }, null)
+                                        .size(100)
+                                        .sorts(
+                                            List.of(
+                                                SortBuilders.fieldSort("nested_value.round"),
+                                                SortBuilders.fieldSort("nested_value.foo"),
+                                                SortBuilders.fieldSort("numeric_value"),
+                                                SortBuilders.fieldSort("_doc")
+                                            )
                                         )
-
                                 )
                         )
-                        .setFetchSource("numeric_value", null),
-                    response -> assertThat(response.getAggregations().get("some_ranges"), notNullValue())
+                        .setFetchSource("numeric_value", null)
+                        .setQuery(new BoolQueryBuilder().filter(new TermQueryBuilder("rare_value", "a"))),
+                    response -> {
+                        var r = Strings.toString((ToXContent) response.getAggregations().get("nested"), true, true);
+                        var prev = agg.getAndSet(r);
+                        if (prev != null) {
+                            assertEquals(prev, r);
+                        }
+                    }
                 );
             }
         }
     }
 
     private static Map<String, ?> randomNestedB(int i, int j) {
-        return Map.of("blub", "blllllaaaa-" + i + "-" + j, "round" + (randomBoolean() ? "" : "-a"), j + 100000);
+        return Map.of("foo", "blllllaaaa-" + i + "-" + j, "round" + (randomBoolean() ? "" : "-a"), j + 100000);
     }
 
     private static Map<String, ?> randomNestedA(int i, int j) {
-        return Map.of("blub", "blllllaaaa-" + i + "-" + j, "round" + (randomBoolean() ? "" : "-b"), j);
+        return Map.of("foo", "blllllaaaa-" + i + "-" + j, "round" + (randomBoolean() ? "" : "-b"), j);
     }
 }
